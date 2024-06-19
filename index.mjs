1183871517594db3fb4d430f967bc91c48f26595
chuans <juaazn.05@gmail.com>
2024-06-19 23:15:46 +0200

    App express

--- index.mjs
+++ index.mjs
@@ -1,10 +1,8 @@
 import express from 'express'
-import { db } from './db/config.mjs'
 import products from './routes/products.mjs'
 import categories from './routes/categories.mjs'
 
 const app = express()
-db.connect()
 
 app.use(express.json())
 app.use('/products', products)
