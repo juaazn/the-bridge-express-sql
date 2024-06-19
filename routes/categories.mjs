d3741a471fdbd8b882872853eba40c017f7aef30
chuans <juaazn.05@gmail.com>
2024-06-19 23:16:29 +0200

    Routes categories

--- routes/categories.mjs
+++ routes/categories.mjs
@@ -1,51 +1,14 @@
 import express from 'express'
-import { db } from '../db/config.mjs'
+import CategoriesController from '../controller/CategoriesController.mjs'
 
 const router = express.Router()
-db.connect()
 
-router.post('/nueva-categoria', (req, res) => {
-  const sql = `INSERT INTO categories SET categoria = '${req.body.categoria}'`
+router.get('/get-categorias', CategoriesController.getCategories)
 
-  db.query(sql, (err, result) => {
-    if (err) throw err
+router.get('/select-categorie/:id_categorie', CategoriesController.getCategoriesForId)
 
-    res.send('New categorie added')
-    console.log(`${result}`)
-  })
-})
+router.post('/nueva-categoria', CategoriesController.postNewCategorie)
 
-router.put('/actualizar-categorie/:id_categorie', (req, res) => {
-  const sql = `UPDATE categories SET categoria = '${req.body.categoria}' WHERE id_categorie = ${req.params.id_categorie}`
-
-  db.query(sql, (err, result) => {
-    if (err) throw err
-
-    res.send('Update categorie')
-    console.log(`${result}`)
-  })
-})
-
-router.get('/get-categorias', (req, res) => {
-  const sql = 'SELECT * FROM categories'
-
-  db.query(sql, (err, result) => {
-    if (err) throw err
-
-    res.send('All categories')
-    console.log(result)
-  })
-})
-
-router.get('/select-categorie/:id_categorie', (req, res) => {
-  const sql = `SELECT * FROM categories WHERE id_categorie = ${req.params.id_categorie}`
-
-  db.query(sql, (err, result) => {
-    if (err) throw err
-
-    res.send('Categorie select for id')
-    console.log(result)
-  })
-})
+router.put('/actualizar-categorie/:id_categorie', CategoriesController.updateCategorie)
 
 export default router
